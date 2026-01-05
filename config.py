437e5232d988650e417a63ff6aa1b8a5d2bb4661
aamandsahu2-code <aamandsahu2@gmail.com>
2026-01-05 15:27:55 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,26 @@
+# ============================================================
+#Group Manager Bot
+# Author: LearningBotsOfficial (https://github.com/LearningBotsOfficial) 
+# Support: https://t.me/LearningBotsCommunity
+# Channel: https://t.me/learning_bots
+# YouTube: https://youtube.com/@learning_bots
+# License: Open-source (keep credits, no resale)
+# ============================================================
+
+import os
+
+# Required configurations (loaded from environment variables)
+API_ID = int(os.getenv("API_ID", 0))
+API_HASH = os.getenv("API_HASH", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+MONGO_URI = os.getenv("MONGO_URI", "")
+DB_NAME = os.getenv("DB_NAME", "Cluster0")
+
+# Owner and bot details
+OWNER_ID = int(os.getenv("OWNER_ID", 0))
+BOT_USERNAME = os.getenv("BOT_USERNAME", "Aira_HelpBot")
+
+# Links and visuals
+SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/+sJeazoQt9zsyZjg9")
+UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL", "https://t.me/+sJeazoQt9zsyZjg9")
+START_IMAGE = os.getenv("START_IMAGE", "https://files.catbox.moe/6y59ir.jpg")
